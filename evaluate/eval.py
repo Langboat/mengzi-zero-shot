0556248e51ff9d37bcf6bfacc5354804be1d6181
JY HUA <JingyunHua1206@gmail.com>
2022-08-10 15:03:49 +0800

    save scores

--- evaluate/eval.py
+++ evaluate/eval.py
@@ -1,12 +1,13 @@
-# from mengzi_zs import MengziZeroShot
-# from mengzi_zs import MengziZeroShot
 import sys
+import time
 from load_data import eprstmt_dataset, tnews_dataset, lcqmc_dataset, cluner_dataset, finre_dataset, cote_dataset, cepsum_dataset, quake_qic_dataset
 from metrics import cal_acc, ner_get_f1_score, cal_f1, rouge_n_corpus_multiple_target
-import warnings
-warnings.filterwarnings("ignore")
+from utils import save_json
 sys.path.append('./')
 from mengzi_zs import MengziZeroShot
+
+import pandas as pd
+pd.options.mode.chained_assignment = None
 
 datasets = {'sentiment_classifier': eprstmt_dataset(),
             'news_classifier': tnews_dataset(),
@@ -17,6 +18,7 @@ datasets = {'sentiment_classifier': eprstmt_dataset(),
             "ad_generation": cepsum_dataset(),
             "medical_domain_intent_classifier": quake_qic_dataset()}
 
+
 mp = MengziZeroShot()  # default
 mp.load()
 
@@ -24,34 +26,35 @@ mp.load()
 DEV_NUM = 32
 
 task_name_list = list(datasets.keys())
-
+results = {}
+time_start = time.time()
 for task_name in task_name_list:
 
     df = datasets[task_name]
     df = df[:DEV_NUM]
 
-    res_list = []
-    if 'input_string2' in list(df.columns):
-        for _, row in df.iterrows():
+    # inference
+    if task_name in ['text_similarity']:
+        for i, row in df.iterrows():
             res = mp.inference(task_type=task_name,
                                input_string=row[0], input_string2=row[1])
-            res_list.append(res)
-    elif 'entity1' in list(df.columns):
-        for _, row in df.iterrows():
+            df.loc[i, 'pred'] = res
+    elif task_name in ["financial_relationship_extraction"]:
+        for i, row in df.iterrows():
             res = mp.inference(
                 task_type=task_name, input_string=row[0], entity1=row[1], entity2=row[2])
-            res_list.append(res)
+            df.loc[i, 'pred'] = res
     else:
-        assert task_name != "text_similarity"
-        for s in list(df['input_string']):
-            res = mp.inference(task_type=task_name, input_string=s)
-            res_list.append(res)
+        for i, row in df.iterrows():
+            res = mp.inference(task_type=task_name, input_string=row[0])
+            df.loc[i, 'pred'] = res
 
-    df['pred'] = res_list
+    results[task_name] = {}
 
     if task_name in ['sentiment_classifier', 'news_classifier', 'text_similarity', "medical_domain_intent_classifier"]:
         res_score = cal_acc(labels=list(df['label']), preds=list(df['pred']))
         print(task_name, f"acc: {res_score}")
+        results[task_name]['acc'] = res_score
 
     elif task_name in ["entity_extraction"]:
         cvt_res = []
@@ -62,22 +65,22 @@ for task_name in task_name_list:
                 content, entity = sub_res.split(":")
                 sub_cvt_res[entity] = content
             cvt_res.append(sub_cvt_res)
-        _, avg = ner_get_f1_score(pre_lines=cvt_res, gold_lines=list(df['label']),
-                                  labels=['地址', '书名', '公司', '游戏', '政府', '电影', '姓名', '组织', '职位', '景点'])
-        print(task_name, f"avg: {avg}")
+        _, f1_score_avg = ner_get_f1_score(pre_lines=cvt_res, gold_lines=list(df['label']), labels=['地址', '书名', '公司', '游戏', '政府', '电影', '姓名', '组织', '职位', '景点'])
+        print(f"{task_name} f1_score_avg: {f1_score_avg}")
+        results[task_name]['f1_score_avg'] = f1_score_avg
 
-    elif task_name in ["financial_relationship_extraction"]:
+    elif task_name in ["financial_relationship_extraction", "comment_object_extraction"]:
         res_score = cal_f1(labels=list(df['label']), preds=list(df['pred']))
-        print(task_name, f"f1: {res_score}")
-
-    elif task_name in ["comment_object_extraction"]:
-        res_score = cal_f1(labels=list(df['label']), preds=list(df['pred']))
-        print(task_name, f"f1: {res_score}")
+        print(f"{task_name}  f1: {res_score}")
+        results[task_name]['f1_score'] = res_score
 
     elif task_name in ["ad_generation"]:
-        res_score = rouge_n_corpus_multiple_target(
-            peers=list(df['label']), models=list(df['pred']))
-        print(task_name, f"rouge_1: {res_score}")
+        res_score = rouge_n_corpus_multiple_target(peers=list(df['label']), models=list(df['pred']))
+        print(f"{task_name} rouge_1: {res_score}")
+        results[task_name]['rouge_1'] = res_score
 
     else:
         raise ValueError("not implement!")
+
+save_json(results)
+print(f'cost time: {round(time.time()-time_start, 2)} s')
